6e9c6bbaf1d5aae189b99cb147a21af7bf596367
Filippo Errico <filipp.rrc@gmail.com>
2020-10-23 19:07:57 +0200

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -1,5 +1,5 @@
 git cms-init
-git clone -b tmp_Ferrico https://github.com/VBF-HZZ/UFHZZAnalysisRun2.git
+git clone -b 10_6_12 https://ferrico@github.com/ferrico/UFHZZAnalysisRun2.git
 git clone -b RunII_v2 https://github.com/VBF-HZZ/UFHZZAnalysisRun2-Accessary.git
 mv UFHZZAnalysisRun2-Accessary/* ./
 rm -rf UFHZZAnalysisRun2-Accessary
